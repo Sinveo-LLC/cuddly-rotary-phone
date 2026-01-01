984123790a7325ec67c696e040e81a66c1613dd7
Sinveo-LLC <244127579+sinveollc@users.noreply.github.com>
2026-01-01 02:32:38 -0600

    Trying some things

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const fileinclude = require('gulp-file-include');
+const paths = {
+  html: {
+    src: 'src/*.html',
+    dest: 'publish/'
+  }
+};
+
+function includeHTML() {
+  return gulp.src(paths.html.src)
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest(paths.html.dest));
+}
+
+// Export the task
+exports.html = includeHTML;
+exports.watch = watchFiles;
+
+function watchFiles() {
+  gulp.watch(['src/*.html', 'src/partials/*.html'], includeHTML);
+}
+
+// Define default task that runs the html task and then starts watching
+exports.default = gulp.series(includeHTML, watchFiles);
